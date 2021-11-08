5699c78e2ce4fee8b6b718a05f26f297db54c1b7
Thomas Habtemariam <84083304+tomhabt@users.noreply.github.com>
2021-11-08 09:27:50 -0500

    payer name prop fixed

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,21 @@
+// function to set name
+var getPlayerName = function() {
+  var name = "";
+
+// ***************************************
+
+while (name === "" || name === null) {
+  name = prompt("What is your robot's name?");
+}
+// ***************************************
+
+  console.log("Your robot's name is " + name);
+  return name;
+};
+
 var playerInfo = {
-  name: window.prompt("What is your robot's name?"),
+  // name: window.prompt("What is your robot's name?"),
+  name: getPlayerName(),
   health: 100,
   attack: 10,
   money: 10,
@@ -27,7 +43,7 @@ var playerInfo = {
     else {
       window.alert("You don't have enough money!");
     }
-  }
+  },
 };
 
 // function to generate a random numeric value between 40 and 60
@@ -114,6 +130,8 @@ var fight = function(enemy) {
   }
 };
 
+
+
 // function to start a new game
 var startGame = function() {
  // reset player stats
@@ -125,6 +143,7 @@ var startGame = function() {
     if (playerInfo.health > 0) {
       // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
       window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+   
 
       // pick new enemy to fight based on the index of the enemyNames array
       var pickedEnemyObj = enemyInfo[i];
